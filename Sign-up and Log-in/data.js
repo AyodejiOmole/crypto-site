a289992ec41adf5ce20a3d055feb9dc8bd6357ad
Omole Daniel <omoledaniel13@gmail.com>
2023-04-26 18:44:53 +0100

    now creates realtime database collection as user signs up

--- Sign-up and Log-in/data.js
+++ Sign-up and Log-in/data.js
@@ -1,7 +1,6 @@
-
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
-import {getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-database.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCaSodrCVZ0LSVhBVgg1kW4DW_Vwg0ewlM",
@@ -15,32 +14,43 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-
-
-
+const database = getDatabase(app);
 
 // const registerName = document.getElementById("nlabel").value;
 
-
 document.getElementById("btn").addEventListener('click', function () {
     console.log("LETS START FROM HERE");
+
     const registerEmail = document.getElementById("eemail").value;
     const registerPassword = document.getElementById("lpassword").value;
+    const registerName = document.getElementById("fname").value;
 
     createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
         .then((userCredential) => {
-            const user = userCredential.user;
+            // const user = userCredential.user;
+            const user = auth.currentUser;
+
+            // const database_ref = database.getReference();
+
+            const user_data = {
+                email: registerEmail,
+                name: registerName,
+                balance: 0
+            }
+
+            // database_ref.child('users/' + user.uid).set(user_data);
+            set(ref(database, 'users/' + user.uid), user_data);
+
             window.location.assign("../bootsrap_portfolio/index-2.html");
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
             console.log(errorMessage);
+            alert(errorMessage);
         });
 
         // window.location.assign("../bootsrap_portfolio/index-2.html");
-        
-
 })
 
 document.getElementById("btn1").addEventListener('click', function () {
@@ -59,10 +69,6 @@ document.getElementById("btn1").addEventListener('click', function () {
             const errorMessage = error.message;
             alert(errorMessage);
         });
-
-        
-        
-
 })
 
-const signInButtons = document.getElementById("users-email").innerHTML=registerEmail;
+// const signInButtons = document.getElementById("users-email").innerHTML=registerEmail;
