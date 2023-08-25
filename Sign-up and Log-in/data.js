2de723c35d9bd0e517cde17f9751d1e56e73e108
AyodejiOmole <omoledaniel13@gmail.com>
2023-08-25 10:50:23 +0100

    email verification link and signal field added

--- Sign-up and Log-in/data.js
+++ Sign-up and Log-in/data.js
@@ -1,6 +1,6 @@
 import { ref, set } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-database.js";
 import { database, auth, len, app } from "../bootsrap_portfolio/js/dashboard/firebase.js"
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, sendEmailVerification } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
 
 // Initialize Firebase authentication and database.
 // const registerName = document.getElementById("nlabel").value;
@@ -46,27 +46,32 @@ document.getElementById("btn").addEventListener('click', function () {
             // const user = userCredential.user;
             const user = auth.currentUser;
 
-            // Creates a collection for each user using an id unique to them in the firebase realtime database as they register using firebase auth. 
-            set(ref(database, 'users2/' + user.uid), {
-                name: registerName,
-                email: registerEmail,
-                phone_number: phone,
-                password: registerPassword,
-                country: countrySelect.value,
-                account_type: accountSelect.value,
-                type: type,
-                balance: 0,
-                profit: 0
-            }).then(() => {
-                // Immediately sets a local storage of the id of the user. This is to enables the dashboard retrieve the details of a particular user using their id that we can now get from local storage.
-                window.localStorage.setItem("id", user.uid);
+            sendEmailVerification(user)
+            .then(() => {
+                window.alert("An email verification link has been sent to your email. Please verify your email before you proceed.");
 
-                // The user is navigated to the dashboard once the collection in the database has been created.  
-                window.location.assign("../bootsrap_portfolio/index-2.html");
-                
-            }).catch((error) => {
-                console.log(error.message);
-            })            
+                // Creates a collection for each user using an id unique to them in the firebase realtime database as they register using firebase auth. 
+                set(ref(database, 'users2/' + user.uid), {
+                    name: registerName,
+                    email: registerEmail,
+                    phone_number: phone,
+                    password: registerPassword,
+                    country: countrySelect.value,
+                    account_type: accountSelect.value,
+                    type: type,
+                    balance: 0,
+                    profit: 0,
+                    signal: ""
+                }).then(() => {
+                    // Immediately sets a local storage of the id of the user. This is to enables the dashboard retrieve the details of a particular user using their id that we can now get from local storage.
+                    window.localStorage.setItem("id", user.uid);
+
+                    // The user is navigated to the dashboard once the collection in the database has been created.  
+                    window.location.assign("../bootsrap_portfolio/index-2.html");
+                }).catch((error) => {
+                    console.log(error.message);
+                })  
+            });          
         })
         .catch((error) => {
             const errorCode = error.code;
