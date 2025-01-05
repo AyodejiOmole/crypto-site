29a0d75399c077c70ab38a2fc1c823bc0d6babce
Omole Ayodeji <omoledaniel13@gmail.com>
2025-01-05 00:00:39 +0000

    Update data.js

--- Sign-up and Log-in/data.js
+++ Sign-up and Log-in/data.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     //     countries.push(country);
     // }
     
-     fetch('./output.json')
+     fetch('../output.json')
       .then(response => {
         if (!response.ok) {
           throw new Error('Failed to load JSON file');
